3c137a080f6ec78b816de9a7d6471d6de4572706
corynathe <corynathe@gmail.com>
2022-02-02 08:02:39 -0600

    Misc code cleanup

--- constants.ts
+++ constants.ts
@@ -1,77 +1,101 @@
 
+export const DIFFICULTY_LEVEL = [
+    {rowCount: 2, colCount: 3, size: 90, name: 'Easy', icons: ['star', 'star-o', 'star-o']},
+    {rowCount: 4, colCount: 3, size: 60, name: 'Medium', icons: ['star', 'star', 'star-o']},
+    {rowCount: 5, colCount: 4, size: 45, name: 'Hard', icons: ['star', 'star', 'star']},
+];
+
 export const COLORS = [
-    'lightblue',
-    'lightgreen',
-    'orange',
-    'pink',
-    'silver',
-    'teal',
-    'violet',
-    'white',
+    'darkblue', '#92f7a8', 'magenta', '#28b59e', 'purple', 'orange', 'lightblue', 'black', 'red',
+    'maroon', 'darkgreen', 'pink', 'tan', 'grey', '#f56c42', '#42ecf5', '#14c93b', '#c996f2',
+    '#4970c4', '#785625'
 ];
 
-export const ICONS = [
-    'baby',
-    'bell',
-    'bone',
-    'broom',
-    'bus',
-    'bug',
-    'camera',
-    'campground',
-    'candy-cane',
-    'cat',
-    'cloud',
-    'coffee',
-    'dice',
-    'dragon',
-    'couch',
-    'crown',
-    'egg',
-    'fish',
-    'hippo',
-    'home',
-    'helicopter',
-    'heart',
-    'ice-cream',
-    'marker',
-    'mitten',
-    'moon',
-    'motorcycle',
-    'music',
-    'paw',
-    'piggy-bank',
-    'plane',
-    'ribbon',
-    'rocket',
-    'ship',
-    'sleigh',
-    'smile',
-    'socks',
-    'spider',
-    'star',
-    'sun',
-    'toolbox',
-    'tractor',
-    'tree',
-    'truck',
-    'water',
-    'snowflake',
-    'snowman',
-    'snowplow',
-];
+export const ICON_GROUP = {
+    egg: {
+        title: 'Scrambled Eggs',
+        playAgain: 'Play Again',
+        iconContainer: {
+            type: 'FontAwesome',
+            name: 'shopping-basket',
+            sizeFactor: 1,
+        },
+        icon: {
+            type: 'MaterialCommunityIcons',
+            name: 'egg-easter',
+        },
+        info: 'Find the egg with the color that matches the basket.',
+    },
+    penguin: {
+        title: 'Penguin Town',
+        playAgain: 'Play Again',
+        iconContainer: {
+            type: 'FontAwesome5',
+            name: 'igloo',
+            sizeFactor: 1,
+        },
+        icon: {
+            type: 'MaterialCommunityIcons',
+            name: 'penguin',
+        },
+        info: 'Find the penguin with the color that matches the igloo.',
+    },
+    book: {
+        title: 'Dewey\'s Decimals',
+        playAgain: 'Play Again',
+        iconContainer: {
+            type: 'MaterialCommunityIcons',
+            name: 'bookshelf',
+            sizeFactor: 1.25,
+        },
+        icon: {
+            type: 'FontAwesome',
+            name: 'book',
+        },
+        info: 'Find the book with the color that matches the shelf.',
+    },
+    baby: {
+        title: 'Diaper Change',
+        playAgain: 'Play Again',
+        iconContainer: {
+            type: 'MaterialCommunityIcons',
+            name: 'baby-carriage',
+            sizeFactor: 1.25,
+        },
+        icon: {
+            type: 'FontAwesome5',
+            name: 'baby',
+        },
+        info: 'Find the baby with the color that matches the stroller.',
+    },
+    cow: {
+        title: 'Moo Moo Mixup',
+        playAgain: 'Play Again',
+        iconContainer: {
+            type: 'MaterialCommunityIcons',
+            name: 'barn',
+            sizeFactor: 1.25,
+        },
+        icon: {
+            type: 'MaterialCommunityIcons',
+            name: 'cow',
+        },
+        info: 'Find the cow with the color that matches the barn.',
+    },
+    ball: {
+        title: 'Hoop-tastic',
+        playAgain: 'Play Again',
+        iconContainer: {
+            type: 'MaterialCommunityIcons',
+            name: 'basketball-hoop-outline',
+            sizeFactor: 1.25,
+        },
+        icon: {
+            type: 'MaterialCommunityIcons',
+            name: 'basketball',
+        },
+        info: 'Find the basketball with the color that matches the hoop.',
+    },
+};
 
-export const APP_ICON = 'rocket';
-export const ITEM_COUNT = 3;
-export const ROW_COUNT = 2;
-export const ACTION_TIMER = 1500;
-export const VOICE_OPTIONS = { rate: 0.75, pitch: 2 };
-export const CORRECT_TEXT = [
-    'Hurray!\nYou found the ',
-    'Great!\nYou found the ',
-    'Amazing!\nYou found the ',
-    'Awesome!\nYou found the ',
-    'Correct!\nYou found the ',
-    'That\'s it!\nYou found the ',
-    'Way to go!\nYou found the ',
-];
+export const TIMEOUT = 1500;
