2d3472239d0439639e12148e2d99d03ba04fb29f
corynathe <corynathe@gmail.com>
2022-02-05 14:21:44 -0600

    Revert title textTransform

--- styles.ts
+++ styles.ts
@@ -26,7 +26,6 @@ export const STYLES = StyleSheet.create({
         fontFamily: 'Papyrus',
         color: '#fff',
         paddingTop: 40,
-        textTransform: 'uppercase'
     },
     infoIcon: {
         paddingRight: 100,
